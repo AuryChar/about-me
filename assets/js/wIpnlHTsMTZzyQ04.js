183a4ec8c8503e61757e96f2d5512405a89d7fee
AuryChar <nat4nz007love@gmail.com>
2025-03-16 04:14:00 -0300

    mhm

--- assets/js/wIpnlHTsMTZzyQ04.js
+++ assets/js/wIpnlHTsMTZzyQ04.js
@@ -1,146 +1,150 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let _0xabc = 0, _0xdef;
-    const _0x123 = document.querySelector('.principal');
-
-    function _0x999() {
-        console.log(Math.random());
-        return false;
-    }
-
-    function _0xaaa() {
-        let _0xbbb = Math.random();
-        console.log(_0xbbb);
-    }
-
-    _0x123.addEventListener('click', function () {
-        _0xabc++;
-        
-        if (_0xabc > 10) {
-            let _0x000 = Math.random();
-            _0x000 = _0x000 + Math.random();
-        }
-
-        if (_0xabc === 3) {
-            while (_0x123.firstChild) {
-                _0x123.removeChild(_0x123.firstChild);
-            }
-            const _0x456 = document.createElement('div');
-            _0x456.innerHTML = '<h1>Ana Banana</h1> <br> <a href="https://www.youtube.com/watch?v=XFkzRNyygfk" target="_blank"><button>yeeaah</button></a>';
-            _0x123.appendChild(_0x456);
-            _0xabc = 0;
-        } else {
-            clearTimeout(_0xdef);
-            _0xdef = setTimeout(() => _0xabc = 0, 500);
-        }
-
-        if (_0xabc < 5) {
-            let _0x111 = Math.random();
-            _0x111++;
-        }
-
-        _0xaaa();
-    });
-
-    if (false) {
-        let _0x777 = 0;
-        _0x777++;
-    }
-
-    let _0x222 = Math.random();
-    let _0x333 = Math.random();
-    let _0x444 = Math.random();
-    let _0x555 = Math.random();
-    let _0x666 = Math.random();
-
-    function _0xbbb() {
-        let _0x888 = Math.random();
-        _0x888++;
-        console.log(_0x888);
-    }
-
-    let _0x777 = Math.random();
-    _0x777 = _0x777 * Math.random();
-
-    if (false) {
-        let _0x9999 = 'Código morto';
-        console.log(_0x9999);
-    }
-
-    function _0xccc() {
-        let _0x0000 = Math.random();
-        _0x0000++;
-        console.log(_0x0000);
-    }
-
-    let _0x1234 = Math.random();
-    _0x1234 = _0x1234 + Math.random();
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    let _0x777 = Math.random();
-    let _0x888 = Math.random();
-    let _0x999 = _0x777 + _0x888;
-
-    if (_0x999 === Math.random()) {
-        console.log(Math.random());
-    }
-
-    let _0x6666 = Math.random();
-    _0x6666++;
-
-    let _0x5555 = Math.random();
-    _0x5555++;
-
-    function _0xddd() {
-        let _0x7777 = Math.random();
-        _0x7777++;
-    }
-
-    _0xddd();
-
-    let _0x00000 = Math.random();
-    _0x00000++;
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    let _0xabcd = Math.random();
+    let _0x1a2b = 0, _0x3c4d = [];
     const _0x5678 = document.querySelector('.principal');
 
     _0x5678.addEventListener('click', function () {
-        _0xabcd++;
-        if (_0xabcd === 3) {
-            while (_0x5678.firstChild) {
-                _0x5678.removeChild(_0x5678.firstChild);
+        const _0x9abc = Date.now();
+        _0x3c4d.push(_0x9abc);
+        _0x1a2b++;
+
+        if (_0x1a2b === 3) {
+            if (_0x3c4d[2] - _0x3c4d[0] <= 1000) {
+                while (_0x5678.firstChild) {
+                    _0x5678.removeChild(_0x5678.firstChild);
+                }
+                const _0xdef0 = document.createElement('div');
+                _0xdef0.innerHTML = '<h1>Ana Banana</h1> <br> <a href="https://www.youtube.com/watch?v=XFkzRNyygfk" target="_blank"><button>yeeaah</button></a>';
+                _0x5678.appendChild(_0xdef0);
             }
-            const _0x9abc = document.createElement('div');
-            _0x9abc.innerHTML = '<h1>Ana Banana</h1> <br> <a href="https://www.youtube.com/watch?v=XFkzRNyygfk" target="_blank"><button>yeeaah</button></a>';
-            _0x5678.appendChild(_0x9abc);
-            _0xabcd = 0;
+            _0x1a2b = 0;
+            _0x3c4d = [];
         } else {
-            clearTimeout(_0xdef);
-            _0xdef = setTimeout(() => _0xabcd = 0, 500);
+            setTimeout(() => {
+                _0x1a2b = 0;
+                _0x3c4d = [];
+            }, 1000);
         }
     });
+
+    let _0x7abf = Math.random();
+    _0x7abf = _0x7abf + Math.random();
+    let _0x0fbc = Math.random();
+    _0x0fbc = _0x0fbc + _0x7abf;
+    let _0x55cd = Math.random();
+    _0x55cd = _0x55cd + _0x7abf + _0x0fbc;
+
+    let _0x4abc = Math.random();
+    let _0x6abc = Math.random();
+    let _0x9abc = Math.random();
+
+    function _0x3cd0() {
+        let _0xabc0 = Math.random();
+        _0xabc0++;
+        _0x9abc = _0xabc0 * _0xabc0;
+    }
+
+    let _0x2abc = Math.random();
+    let _0x6f23 = Math.random();
+    let _0x1cd4 = _0x6f23 + _0x2abc;
+
+    function _0x34ac() {
+        let _0x7cd5 = Math.random();
+        _0x7cd5++;
+    }
+
+    _0x34ac();
+
+    function _0x2bcd() {
+        let _0x6a7d = Math.random();
+        _0x6a7d++;
+        _0x4abc = _0x6a7d * _0x7abf;
+    }
+
+    _0x3cd0();
+    _0x2bcd();
+    _0x34ac();
+
+    let _0x8e39 = Math.random();
+    _0x8e39 = _0x8e39 * Math.random();
+    let _0x8a74 = Math.random();
+    let _0x9ab5 = _0x8a74 * _0x8e39;
+
+    function _0x6a89() {
+        let _0x7bcd = Math.random();
+        _0x7bcd++;
+    }
+
+    _0x6a89();
+
+    let _0x4db9 = Math.random();
+    _0x4db9 = _0x4db9 * Math.random();
+    let _0x5bc9 = _0x4db9 + Math.random();
+    let _0x1b87 = _0x5bc9 + Math.random();
+
+    function _0x9c35() {
+        let _0x8a12 = Math.random();
+        _0x8a12++;
+        console.log(_0x8a12);
+    }
+
+    let _0x7abc = Math.random();
+    let _0x1c23 = Math.random();
+    let _0x1f93 = _0x7abc + _0x1c23;
+    let _0x5671 = Math.random();
+
+    if (Math.random() > 0.5) {
+        _0x9c35();
+    }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    let _0xabc = 0;
-    const _0x123 = document.querySelector('.principal');
+    let _0x1a37 = Math.random();
+    let _0x3b28 = Math.random();
+    let _0x8a49 = _0x1a37 + _0x3b28;
+    let _0x0c9b = Math.random();
+    let _0x7e47 = _0x0c9b + Math.random();
+    let _0x5f3b = Math.random();
 
-    _0x123.addEventListener('click', function () {
-        _0xabc++;
+    function _0x12fa() {
+        let _0x2a49 = Math.random();
+        _0x2a49++;
+    }
 
-        if (_0xabc === 3) {
-            while (_0x123.firstChild) {
-                _0x123.removeChild(_0x123.firstChild);
+    let _0x9d22 = Math.random();
+    _0x9d22 = _0x9d22 * Math.random();
+    let _0x7bbf = Math.random();
+    let _0x89fa = _0x7bbf * _0x9d22;
+
+    _0x12fa();
+
+    function _0x23f7() {
+        let _0x1e6d = Math.random();
+        _0x1e6d++;
+        _0x1f93 = _0x1e6d * _0x1f93;
+    }
+
+    _0x23f7();
+    _0x7e47++;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    let _0x1bb2 = Math.random();
+    const _0x5c78 = document.querySelector('.principal');
+
+    _0x5c78.addEventListener('click', function () {
+        _0x1bb2++;
+        if (_0x1bb2 === 3) {
+            while (_0x5c78.firstChild) {
+                _0x5c78.removeChild(_0x5c78.firstChild);
             }
-            const _0x456 = document.createElement('div');
-            _0x456.innerHTML = '<h1>Ana Banana</h1> <br> <a href="https://www.youtube.com/watch?v=XFkzRNyygfk" target="_blank"><button>sei q vc gosta dessa musica</button></a>';
-            _0x123.appendChild(_0x456);
-            _0xabc = 0;
+            const _0x74d3 = document.createElement('div');
+            _0x74d3.innerHTML = '<h1>Ana Banana</h1> <br> <a href="https://www.youtube.com/watch?v=XFkzRNyygfk" target="_blank"><button>yeeaah</button></a>';
+            _0x5c78.appendChild(_0x74d3);
+            _0x1bb2 = 0;
         } else {
-            clearTimeout(_0xdef);
-            _0xdef = setTimeout(() => _0xabc = 0, 500);
+            setTimeout(() => {
+                _0x1bb2 = 0;
+            }, 1000);
         }
     });
 });
