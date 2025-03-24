75461ed09dcd75784ee24223ac08ecbbab4d05ff
AuryChar <nat4nz007love@gmail.com>
2025-03-24 19:51:03 -0300

    Back button and new color

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,3 +2,9 @@ const principal = document.querySelector('.principal');
 setTimeout(() => {
     principal.classList.add('apears');
 }, 500);
+
+const backBtn = document.querySelector('#back-page');
+
+backBtn.addEventListener('click', () => {
+    window.history.back();
+});
